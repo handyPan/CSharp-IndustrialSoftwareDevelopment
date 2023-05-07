24a6be560c0de749fa90b509bd965a340e30513c
handyPan <courseware.cs@gmail.com>
2023-05-07 14:29:20 -0400

    ex02-ep4 CountDownApp completed

---
+++ ex02-C#上位机开发串口通信编程/CountDownApp/CountDownApp/frmMain.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace CountDownApp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmb_specifiedTime = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_remainingTime = new System.Windows.Forms.Label();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.progBar_completedTime = new System.Windows.Forms.ProgressBar();
+            this.tmr_interval = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Specified";
+            // 
+            // cmb_specifiedTime
+            // 
+            this.cmb_specifiedTime.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmb_specifiedTime.FormattingEnabled = true;
+            this.cmb_specifiedTime.Location = new System.Drawing.Point(121, 39);
+            this.cmb_specifiedTime.Name = "cmb_specifiedTime";
+            this.cmb_specifiedTime.Size = new System.Drawing.Size(121, 20);
+            this.cmb_specifiedTime.TabIndex = 1;
+            this.cmb_specifiedTime.DropDownClosed += new System.EventHandler(this.cmb_specifiedTime_DropDownClosed);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Remaining";
+            // 
+            // lbl_remainingTime
+            // 
+            this.lbl_remainingTime.AutoSize = true;
+            this.lbl_remainingTime.Location = new System.Drawing.Point(119, 85);
+            this.lbl_remainingTime.Name = "lbl_remainingTime";
+            this.lbl_remainingTime.Size = new System.Drawing.Size(41, 12);
+            this.lbl_remainingTime.TabIndex = 0;
+            this.lbl_remainingTime.Text = "0 secs";
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Location = new System.Drawing.Point(299, 35);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 2;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // progBar_completedTime
+            // 
+            this.progBar_completedTime.Location = new System.Drawing.Point(32, 133);
+            this.progBar_completedTime.Name = "progBar_completedTime";
+            this.progBar_completedTime.Size = new System.Drawing.Size(342, 23);
+            this.progBar_completedTime.TabIndex = 3;
+            // 
+            // tmr_interval
+            // 
+            this.tmr_interval.Interval = 1000;
+            this.tmr_interval.Tick += new System.EventHandler(this.tmr_interval_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 195);
+            this.Controls.Add(this.progBar_completedTime);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.cmb_specifiedTime);
+            this.Controls.Add(this.lbl_remainingTime);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Text = "Count Down App";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmb_specifiedTime;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_remainingTime;
+        private System.Windows.Forms.Button btn_Start;
+        private System.Windows.Forms.ProgressBar progBar_completedTime;
+        private System.Windows.Forms.Timer tmr_interval;
+    }
+}
+
---
+++ ex02-C#上位机开发串口通信编程/CountDownApp/CountDownApp/frmMain.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CountDownApp
+{
+    public partial class frmMain : Form
+    {
+        int cnt;
+        int remainingTime;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            for (int i=1;i<=30;i++)
+            {
+                cmb_specifiedTime.Items.Add(i.ToString() + " secs");
+            }
+            cmb_specifiedTime.Text = "5 secs";
+            btn_Start.Enabled = true;
+            lbl_remainingTime.Text = "";
+        }
+
+        private void tmr_interval_Tick(object sender, EventArgs e)
+        {
+            cnt++;
+            lbl_remainingTime.Text = (remainingTime - cnt).ToString() + " secs";
+            progBar_completedTime.Value = cnt;
+            if (cnt == remainingTime)
+            {
+                tmr_interval.Stop();
+                System.Media.SystemSounds.Asterisk.Play();
+                btn_Start.Enabled = false;
+                btn_Start.Text = "Start";
+                cmb_specifiedTime.Enabled = true;
+                MessageBox.Show("Time is up!", "Alert");
+            }
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            string str = cmb_specifiedTime.Text;
+            remainingTime = Convert.ToInt16(str.Substring(0, 2));
+            progBar_completedTime.Value = 0;
+            if (btn_Start.Text == "Start")
+            {
+                
+                progBar_completedTime.Maximum = remainingTime;
+                btn_Start.Text = "Cancel";
+                cmb_specifiedTime.Enabled = false;
+                tmr_interval.Start();
+            }
+            else
+            {
+                cnt = 0;
+                progBar_completedTime.Maximum = 0;
+                btn_Start.Text = "Start";
+                cmb_specifiedTime.Enabled = true;
+                lbl_remainingTime.Text = "0 secs";
+                tmr_interval.Stop();
+            }
+        }
+
+        private void cmb_specifiedTime_DropDownClosed(object sender, EventArgs e)
+        {
+            cnt = 0;
+            if (cmb_specifiedTime.Text != "")
+            {
+                btn_Start.Enabled = true;
+            }
+        }
+    }
+}
