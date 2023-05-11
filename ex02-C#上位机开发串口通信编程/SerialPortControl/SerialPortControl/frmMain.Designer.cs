b7abec5d66a332c749fc8ac68b0d1febf62ac30f
handyPan <courseware.cs@gmail.com>
2023-05-11 15:10:12 -0400

    ex02-ep11 completed

--- ex02-C#上位机开发串口通信编程/SerialPortControl/SerialPortControl/frmMain.Designer.cs
+++ ex02-C#上位机开发串口通信编程/SerialPortControl/SerialPortControl/frmMain.Designer.cs
@@ -35,7 +35,10 @@
             this.btn_closeDevice1 = new System.Windows.Forms.Button();
             this.btn_openDevice1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txt_timerToTurnOff = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.btn_closeDevice2 = new System.Windows.Forms.Button();
             this.btn_openDevice2 = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
@@ -43,6 +46,12 @@
             this.btn_openDevice3 = new System.Windows.Forms.Button();
             this.btn_closeDevice3 = new System.Windows.Forms.Button();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lbl_device1CloseAfter = new System.Windows.Forms.Label();
+            this.lbl_device1Status = new System.Windows.Forms.Label();
+            this.lbl_device2Status = new System.Windows.Forms.Label();
+            this.lbl_device3Status = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
@@ -69,17 +78,22 @@
             // 
             // btn_openSerialPort
             // 
-            this.btn_openSerialPort.Location = new System.Drawing.Point(258, 36);
+            this.btn_openSerialPort.BackgroundImage = global::SerialPortControl.Properties.Resources.btn_dark;
+            this.btn_openSerialPort.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_openSerialPort.Location = new System.Drawing.Point(369, 15);
             this.btn_openSerialPort.Name = "btn_openSerialPort";
-            this.btn_openSerialPort.Size = new System.Drawing.Size(184, 23);
+            this.btn_openSerialPort.Size = new System.Drawing.Size(79, 39);
             this.btn_openSerialPort.TabIndex = 1;
-            this.btn_openSerialPort.Text = "Open Serial Port";
+            this.btn_openSerialPort.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
             this.btn_openSerialPort.UseVisualStyleBackColor = true;
             this.btn_openSerialPort.Click += new System.EventHandler(this.btn_openSerialPort_Click);
+            this.btn_openSerialPort.MouseLeave += new System.EventHandler(this.btn_openSerialPort_MouseLeave);
+            this.btn_openSerialPort.MouseHover += new System.EventHandler(this.btn_openSerialPort_MouseHover);
             // 
             // btn_closeDevice1
             // 
-            this.btn_closeDevice1.Location = new System.Drawing.Point(238, 25);
+            this.btn_closeDevice1.Enabled = false;
+            this.btn_closeDevice1.Location = new System.Drawing.Point(266, 20);
             this.btn_closeDevice1.Name = "btn_closeDevice1";
             this.btn_closeDevice1.Size = new System.Drawing.Size(184, 23);
             this.btn_closeDevice1.TabIndex = 1;
@@ -89,7 +103,7 @@
             // 
             // btn_openDevice1
             // 
-            this.btn_openDevice1.Location = new System.Drawing.Point(19, 25);
+            this.btn_openDevice1.Location = new System.Drawing.Point(19, 20);
             this.btn_openDevice1.Name = "btn_openDevice1";
             this.btn_openDevice1.Size = new System.Drawing.Size(184, 23);
             this.btn_openDevice1.TabIndex = 1;
@@ -99,27 +113,59 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btn_openSerialPort);
             this.groupBox1.Location = new System.Drawing.Point(20, 12);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(438, 65);
+            this.groupBox1.Size = new System.Drawing.Size(471, 65);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Serial Port";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(269, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Turn ON/OFF";
+            // 
             // groupBox2
             // 
+            this.groupBox2.Controls.Add(this.lbl_device1Status);
+            this.groupBox2.Controls.Add(this.txt_timerToTurnOff);
+            this.groupBox2.Controls.Add(this.lbl_device1CloseAfter);
+            this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.btn_openDevice1);
             this.groupBox2.Controls.Add(this.btn_closeDevice1);
             this.groupBox2.Location = new System.Drawing.Point(20, 92);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(438, 65);
+            this.groupBox2.Size = new System.Drawing.Size(471, 91);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Device 1";
             // 
+            // txt_timerToTurnOff
+            // 
+            this.txt_timerToTurnOff.Location = new System.Drawing.Point(166, 54);
+            this.txt_timerToTurnOff.Name = "txt_timerToTurnOff";
+            this.txt_timerToTurnOff.Size = new System.Drawing.Size(37, 21);
+            this.txt_timerToTurnOff.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Timer to turn off (sec)";
+            // 
             // btn_closeDevice2
             // 
-            this.btn_closeDevice2.Location = new System.Drawing.Point(238, 25);
+            this.btn_closeDevice2.Enabled = false;
+            this.btn_closeDevice2.Location = new System.Drawing.Point(264, 25);
             this.btn_closeDevice2.Name = "btn_closeDevice2";
             this.btn_closeDevice2.Size = new System.Drawing.Size(184, 23);
             this.btn_closeDevice2.TabIndex = 1;
@@ -139,22 +185,24 @@
             // 
             // groupBox3
             // 
+            this.groupBox3.Controls.Add(this.lbl_device2Status);
             this.groupBox3.Controls.Add(this.btn_openDevice2);
             this.groupBox3.Controls.Add(this.btn_closeDevice2);
-            this.groupBox3.Location = new System.Drawing.Point(20, 172);
+            this.groupBox3.Location = new System.Drawing.Point(20, 199);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(438, 65);
+            this.groupBox3.Size = new System.Drawing.Size(471, 65);
             this.groupBox3.TabIndex = 4;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Device 2";
             // 
             // groupBox4
             // 
+            this.groupBox4.Controls.Add(this.lbl_device3Status);
             this.groupBox4.Controls.Add(this.btn_openDevice3);
             this.groupBox4.Controls.Add(this.btn_closeDevice3);
-            this.groupBox4.Location = new System.Drawing.Point(20, 254);
+            this.groupBox4.Location = new System.Drawing.Point(20, 281);
             this.groupBox4.Name = "groupBox4";
-            this.groupBox4.Size = new System.Drawing.Size(438, 65);
+            this.groupBox4.Size = new System.Drawing.Size(471, 65);
             this.groupBox4.TabIndex = 5;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Device 3";
@@ -171,7 +219,8 @@
             // 
             // btn_closeDevice3
             // 
-            this.btn_closeDevice3.Location = new System.Drawing.Point(238, 25);
+            this.btn_closeDevice3.Enabled = false;
+            this.btn_closeDevice3.Location = new System.Drawing.Point(264, 25);
             this.btn_closeDevice3.Name = "btn_closeDevice3";
             this.btn_closeDevice3.Size = new System.Drawing.Size(184, 23);
             this.btn_closeDevice3.TabIndex = 1;
@@ -179,22 +228,62 @@
             this.btn_closeDevice3.UseVisualStyleBackColor = true;
             this.btn_closeDevice3.Click += new System.EventHandler(this.btn_closeDevice3_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lbl_device1CloseAfter
+            // 
+            this.lbl_device1CloseAfter.AutoSize = true;
+            this.lbl_device1CloseAfter.Location = new System.Drawing.Point(269, 55);
+            this.lbl_device1CloseAfter.Name = "lbl_device1CloseAfter";
+            this.lbl_device1CloseAfter.Size = new System.Drawing.Size(137, 12);
+            this.lbl_device1CloseAfter.TabIndex = 2;
+            this.lbl_device1CloseAfter.Text = "Device 1 closes after ";
+            // 
+            // lbl_device1Status
+            // 
+            this.lbl_device1Status.Location = new System.Drawing.Point(214, 20);
+            this.lbl_device1Status.Name = "lbl_device1Status";
+            this.lbl_device1Status.Size = new System.Drawing.Size(25, 25);
+            this.lbl_device1Status.TabIndex = 4;
+            this.lbl_device1Status.Text = "label3";
+            // 
+            // lbl_device2Status
+            // 
+            this.lbl_device2Status.Location = new System.Drawing.Point(214, 25);
+            this.lbl_device2Status.Name = "lbl_device2Status";
+            this.lbl_device2Status.Size = new System.Drawing.Size(25, 25);
+            this.lbl_device2Status.TabIndex = 4;
+            this.lbl_device2Status.Text = "label3";
+            // 
+            // lbl_device3Status
+            // 
+            this.lbl_device3Status.Location = new System.Drawing.Point(214, 23);
+            this.lbl_device3Status.Name = "lbl_device3Status";
+            this.lbl_device3Status.Size = new System.Drawing.Size(25, 25);
+            this.lbl_device3Status.TabIndex = 4;
+            this.lbl_device3Status.Text = "label3";
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(478, 332);
+            this.ClientSize = new System.Drawing.Size(514, 365);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
-            this.Controls.Add(this.btn_openSerialPort);
             this.Controls.Add(this.btn_scan);
             this.Controls.Add(this.cmb_serialPort);
             this.Controls.Add(this.groupBox1);
             this.Name = "frmMain";
             this.Text = "Serial Port Control";
             this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.groupBox3.ResumeLayout(false);
             this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
@@ -217,6 +306,14 @@
         private System.Windows.Forms.Button btn_openDevice3;
         private System.Windows.Forms.Button btn_closeDevice3;
         private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_timerToTurnOff;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl_device1CloseAfter;
+        private System.Windows.Forms.Label lbl_device1Status;
+        private System.Windows.Forms.Label lbl_device2Status;
+        private System.Windows.Forms.Label lbl_device3Status;
     }
 }
 
--- ex02-C#上位机开发串口通信编程/SerialPortControl/SerialPortControl/frmMain.cs
+++ ex02-C#上位机开发串口通信编程/SerialPortControl/SerialPortControl/frmMain.cs
@@ -25,6 +25,13 @@ namespace SerialPortControl
         // Serial Port write buffer
         byte[] serialPortDataBuffer = new byte[1];
 
+        bool btnOpenSerialPortState;
+
+        // configured timer1 value
+        int timer1ConfiguredSec;
+        // timer ticked seconds
+        int timer1TickedSec;
+
         public frmMain()
         {
             InitializeComponent();
@@ -37,13 +44,40 @@ namespace SerialPortControl
                 try
                 {
                     serialPort1.Close();
+                    // close devices if turned ON
+                    if (lbl_device1Status.BackColor == Color.DarkGreen)
+                    {
+                        timer1.Stop();
+                        btn_openDevice1.Enabled = true;
+                        btn_closeDevice1.Enabled = false;
+                        lbl_device1Status.BackColor = Color.DarkGray;
+                        lbl_device1CloseAfter.Text = "";
+                        timer1TickedSec = 0;
+                        writeByteToSerialPort(deviceClose1);
+                    }
+                    if (lbl_device2Status.BackColor == Color.DarkGreen)
+                    {
+                        btn_openDevice2.Enabled = true;
+                        btn_closeDevice2.Enabled = false;
+                        writeByteToSerialPort(deviceClose2);
+                        lbl_device2Status.BackColor = Color.DarkGray;
+                    }
+                    if (lbl_device3Status.BackColor == Color.DarkGreen)
+                    {
+                        btn_openDevice3.Enabled = true;
+                        btn_closeDevice3.Enabled = false;
+                        writeByteToSerialPort(deviceClose3);
+                        lbl_device3Status.BackColor = Color.DarkGray;
+                    }
                 }
                 catch (Exception)
                 {
 
                     throw;
                 }
-                btn_openSerialPort.Text = "Open Serial Port";
+                // btn_openSerialPort.Text = "Open Serial Port";
+                btn_openSerialPort.BackgroundImage = Properties.Resources.btn_dark;
+                btnOpenSerialPortState = false;
             }
             else
             {
@@ -51,7 +85,9 @@ namespace SerialPortControl
                 {
                     serialPort1.PortName = cmb_serialPort.Text;
                     serialPort1.Open();
-                    btn_openSerialPort.Text = "Close Serial Port";
+                    // btn_openSerialPort.Text = "Close Serial Port";
+                    btn_openSerialPort.BackgroundImage = Properties.Resources.btn_green;
+                    btnOpenSerialPortState = true;
                 }
                 catch (Exception)
                 {
@@ -61,9 +97,40 @@ namespace SerialPortControl
             }
         }
 
-        private void frmMain_Load(object sender, EventArgs e)
+        private void btn_openSerialPort_MouseLeave(object sender, EventArgs e)
         {
+            if (btnOpenSerialPortState)
+            {
+                btn_openSerialPort.BackgroundImage = Properties.Resources.btn_green;
+            }
+            else
+            {
+                btn_openSerialPort.BackgroundImage = Properties.Resources.btn_dark;
+            }
+        }
+
+        private void btn_openSerialPort_MouseHover(object sender, EventArgs e)
+        {
+            btn_openSerialPort.BackgroundImage = Properties.Resources.btn_lightgreen;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {   
+            // initialize serial port list
             searchAndAddSerialPortToComboBox(serialPort1, cmb_serialPort);
+            // draw device status lights
+            var path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddEllipse(0, 0, 22, 22);
+            lbl_device1Status.Text = "";
+            lbl_device1Status.Region = new Region(path);
+            lbl_device1Status.BackColor = Color.DarkGray;
+            lbl_device1CloseAfter.Text = "";
+            lbl_device2Status.Text = "";
+            lbl_device2Status.Region = new Region(path);
+            lbl_device2Status.BackColor = Color.DarkGray;
+            lbl_device3Status.Text = "";
+            lbl_device3Status.Region = new Region(path);
+            lbl_device3Status.BackColor = Color.DarkGray;
         }
 
         private void searchAndAddSerialPortToComboBox(SerialPort myPort, ComboBox myBox)
@@ -96,33 +163,101 @@ namespace SerialPortControl
         }
 
         private void btn_openDevice1_Click(object sender, EventArgs e)
-        {
-            writeByteToSerialPort(deviceOpen1);
+        {   
+            if (!btnOpenSerialPortState)
+            {
+                MessageBox.Show("Open the serial port to proceed.");
+                return;
+            }
+
+            if (txt_timerToTurnOff.Text == "")
+            {
+                MessageBox.Show("The timer value can't be empty.");
+                return;
+            }
+
+            // try parsing the input timer value
+            try
+            {
+                if (int.TryParse(txt_timerToTurnOff.Text, out timer1ConfiguredSec))
+                {
+                    if (timer1ConfiguredSec > 0)
+                    {
+                        timer1.Start();
+                        btn_openDevice1.Enabled = false;
+                        btn_closeDevice1.Enabled = true;
+                        lbl_device1Status.BackColor = Color.DarkGreen;
+                    }
+                    else
+                    {
+                        MessageBox.Show("The timer value should be larger than 0.");
+                        return;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("The timer value should be an integer.");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("The timer value should be an integer.");
+                return;
+            }
         }
 
         private void btn_closeDevice1_Click(object sender, EventArgs e)
         {
+            timer1.Stop();
             writeByteToSerialPort(deviceClose1);
+            btn_openDevice1.Enabled = true;
+            btn_closeDevice1.Enabled = false;
+            lbl_device1Status.BackColor = Color.DarkGray;
+            lbl_device1CloseAfter.Text = "";
+            timer1TickedSec = 0;
         }
 
         private void btn_openDevice2_Click(object sender, EventArgs e)
         {
+            if (!btnOpenSerialPortState)
+            {
+                MessageBox.Show("Open the serial port to proceed.");
+                return;
+            }
             writeByteToSerialPort(deviceOpen2);
+            btn_openDevice2.Enabled = false;
+            lbl_device2Status.BackColor = Color.DarkGreen;
+            btn_closeDevice2.Enabled = true;
         }
 
         private void btn_closeDevice2_Click(object sender, EventArgs e)
         {
             writeByteToSerialPort(deviceClose2);
+            btn_openDevice2.Enabled = true;
+            btn_closeDevice2.Enabled = false;
+            lbl_device2Status.BackColor = Color.DarkGray;
         }
 
         private void btn_openDevice3_Click(object sender, EventArgs e)
         {
+            if (!btnOpenSerialPortState)
+            {
+                MessageBox.Show("Open the serial port to proceed.");
+                return;
+            }
             writeByteToSerialPort(deviceOpen3);
+            btn_openDevice3.Enabled = false;
+            lbl_device3Status.BackColor = Color.DarkGreen;
+            btn_closeDevice3.Enabled = true;
         }
 
         private void btn_closeDevice3_Click(object sender, EventArgs e)
         {
             writeByteToSerialPort(deviceClose3);
+            btn_openDevice3.Enabled = true;
+            btn_closeDevice3.Enabled = false;
+            lbl_device3Status.BackColor = Color.DarkGray;
         }
 
         private void writeByteToSerialPort(byte data)   
@@ -144,6 +279,22 @@ namespace SerialPortControl
         private void btn_scan_Click(object sender, EventArgs e)
         {
             searchAndAddSerialPortToComboBox(serialPort1, cmb_serialPort);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1TickedSec++;
+            lbl_device1CloseAfter.Text = $"Device 1 closes after {timer1ConfiguredSec - timer1TickedSec} seconds.";
+            // close device 1 if remaining time is 0
+            if (timer1ConfiguredSec == timer1TickedSec)
+            {
+                timer1.Stop();
+                writeByteToSerialPort(deviceClose1);
+                btn_openDevice1.Enabled = true;
+                lbl_device1Status.BackColor = Color.DarkGray;
+                lbl_device1CloseAfter.Text = "";
+                timer1TickedSec = 0;
+            }
         }
     }
 }
